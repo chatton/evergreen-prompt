fe92154fad05cb9584ca7feb8acdc59acf1674b1
chatton <cianhatton@gmail.com>
2021-03-27 13:51:19 +0000

    added executor.go

---
+++ pkg/evgprompt/executor.go
@@ -0,0 +1,34 @@
+package evgprompt
+
+import (
+	"chatton.com/evergreen-prompt/pkg/evergreen/client"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Executor struct {
+	client *client.EvergreenClient
+}
+
+func NewExecutor(client *client.EvergreenClient) *Executor {
+	return &Executor{client: client}
+}
+
+func (e *Executor) Execute(in string) {
+	if in == "" {
+		return
+	}
+	if in == "quit" || in == "exit" {
+		fmt.Printf("Bye!")
+		os.Exit(0)
+	}
+
+	if strings.HasPrefix(in, "set-project") {
+		split := strings.Split(in, " ")
+		project := split[1]
+		fmt.Println("You selected project: " + project)
+	}
+}
+
+
